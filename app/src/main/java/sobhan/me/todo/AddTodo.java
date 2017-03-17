a71d0b1bd15af8180e5727243b24de7fb935feba
Sobhan Bagheri <Sobhan Bagheri>
2017-03-17 18:04:55 +0330

    Making add todo activity ready

--- app/src/main/java/sobhan/me/todo/AddTodo.java
+++ app/src/main/java/sobhan/me/todo/AddTodo.java
@@ -2,6 +2,8 @@ package sobhan.me.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class AddTodo extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AddTodo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
+    }
+
+    public void savetodo(View v)
+    {
+        Toast.makeText(this, "Hey", Toast.LENGTH_LONG).show();
     }
 }
