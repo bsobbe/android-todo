7648bc99254f573e34177ff3aa3a7cbd0e9ad7aa
Sobhan Bagheri <Sobhan Bagheri>
2017-03-17 17:31:44 +0330

    Method for add has been created

--- app/src/main/java/sobhan/me/todo/MainActivity.java
+++ app/src/main/java/sobhan/me/todo/MainActivity.java
@@ -2,6 +2,8 @@ package sobhan.me.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newTodo(View v)
+    {
+        Toast.makeText(this, "hey", Toast.LENGTH_LONG).show();
     }
 
 }
