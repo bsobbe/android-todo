29b92769eb827c914ac23c6170ae0bce1d2b6e57
Sobhan Bagheri <Sobhan Bagheri>
2017-03-17 13:02:53 +0330

    Setting on click listener and changing the id of quick add button

---
+++ app/src/main/java/sobhan/me/todo/AddTodo.java
@@ -0,0 +1,13 @@
+package sobhan.me.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+    }
+}
--- app/src/main/java/sobhan/me/todo/MainActivity.java
+++ app/src/main/java/sobhan/me/todo/MainActivity.java
@@ -3,6 +3,7 @@ package sobhan.me.todo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.ramotion.foldingcell.FoldingCell;
 
@@ -19,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 fc.toggle(false);
+            }
+        });
+
+        Button addTodo = (Button) findViewById(R.id.add_todo);
+        addTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
             }
         });
     }
