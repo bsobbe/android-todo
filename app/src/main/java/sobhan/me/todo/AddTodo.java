b743077f6a6cc2b4e42da43f9a3420fb0ff86f5a
Sobhan Bagheri <Sobhan Bagheri>
2017-03-17 21:42:10 +0330

    Adding todo, to the list of data

--- app/src/main/java/sobhan/me/todo/AddTodo.java
+++ app/src/main/java/sobhan/me/todo/AddTodo.java
@@ -1,8 +1,10 @@
 package sobhan.me.todo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddTodo extends AppCompatActivity {
@@ -13,8 +15,15 @@ public class AddTodo extends AppCompatActivity {
         setContentView(R.layout.activity_add_todo);
     }
 
-    public void savetodo(View v)
+    public void saveTodo(View v)
     {
-        Toast.makeText(this, "Hey", Toast.LENGTH_LONG).show();
+        EditText editTextContent = (EditText) findViewById(R.id.todo_content);
+        String content = editTextContent.getText().toString();
+        if (!content.equals("")) {
+            Intent intent  = new Intent();
+            intent.putExtra("content", content);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/sobhan/me/todo/MainActivity.java
+++ app/src/main/java/sobhan/me/todo/MainActivity.java
@@ -1,21 +1,46 @@
 package sobhan.me.todo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+    public final static int NEW_TODO_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
     }
 
     public void newTodo(View v)
     {
-        Toast.makeText(this, "hey", Toast.LENGTH_LONG).show();
+        Intent intent =  new Intent(MainActivity.this, AddTodo.class);
+        startActivityForResult(intent, NEW_TODO_REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1 && resultCode ==  RESULT_OK) {
+            String content = data.getStringExtra("content");
+            arrayList.add(content);
+            arrayAdapter.notifyDataSetChanged();
+        }
     }
 
 }
