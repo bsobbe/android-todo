7a162dfb700feb988673c7cd0d2f10690be5a709
Sobhan Bagheri <Sobhan Bagheri>
2017-03-17 16:23:39 +0330

    Folding cell has been deleted from the project

--- app/src/main/java/sobhan/me/todo/MainActivity.java
+++ app/src/main/java/sobhan/me/todo/MainActivity.java
@@ -2,10 +2,6 @@ package sobhan.me.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.ramotion.foldingcell.FoldingCell;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,22 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Setting up the foldingCell for animation.
-        final FoldingCell fc = (FoldingCell) findViewById(R.id.folding_cell);
-        fc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fc.toggle(false);
-            }
-        });
-
-        Button addTodo = (Button) findViewById(R.id.add_todo);
-        addTodo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
     }
 }
