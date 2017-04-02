4217159e4f7b3c18d7f79c4678ea45bb2f6cd8a7
Sobhan Bagheri <Sobhan Bagheri>
2017-04-02 16:39:00 +0430

    Trigger toast after selecting about me

--- app/src/main/java/sobhan/me/todo/MainActivity.java
+++ app/src/main/java/sobhan/me/todo/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -85,10 +86,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case R.id.menu_about_me:
+                Toast.makeText(MainActivity.this, "about me", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void newTodo(View v)
